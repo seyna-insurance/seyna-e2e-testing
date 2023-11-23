071f16772b1259ccd38a906909e0314efad05a78
Guillaume Font <guillaume.font@gmail.com>
2023-11-23 15:30:21 +0100

    feat(tests) Add mobile browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,24 +49,25 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 13'] },
+    },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: "Firefox",
+      use: { ...devices['Desktop Firefox']}
+    }
   ],
 
   /* Run your local dev server before starting the tests */
